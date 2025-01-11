671a8c9aa36ebdba047cfae51b18f187354d67e7
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2025-01-11 14:33:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const token = '7827921460:AAEkT715nf6B1FIWLu-E1PypfVak6KiSBG4';
 const bot = new TelegramBot(token, { polling: true });
 
 // Load the plugins
-const hiP = require('./plugins/hiPl');
+const hiP = require('./plugins/hiP');
 hiPlugin(bot); // Load hiPlugin
 
 const buttonP= require('./plugins/buttonP');
